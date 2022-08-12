36ec8b0979d3d6bb3354d2178b2939f4ecc2b0f3
aslikandaz <79531642+aslikandaz@users.noreply.github.com>
2022-08-12 20:12:18 +0300

    Add project files.

---
+++ PayCore-H1/Models/InterestResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayCore_H1.Models
+{
+    public class InterestResponseModel
+    //created class to return results
+    {
+        public double InterestRate { get; set; }//faiz oranı
+        public int InterestYield { get; set; }//faiz getirisi
+        public int TotalBalance { get; set; }//toplam para
+    }
+}
